666f1d4923f9ef85bfb04aa9b057d1650bb38c6b
Parimal Deshmukh <pdeshmukh@equalexperts.com>
2013-03-16 12:06:58 +0000

    added mockito maven dependency, working on first test

--- src/main/java/org/wrapdota/server/SteamServer.java
+++ src/main/java/org/wrapdota/server/SteamServer.java
@@ -3,7 +3,6 @@ package org.wrapdota.server;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.wrapdota.game.DotaMatch;
 
 import java.io.IOException;
@@ -12,15 +11,15 @@ import java.util.List;
 public class SteamServer {
 
     String API_KEY;
-    String TEST_API_URL="IDOTA2Match_205790";
-    String API_URL="IDOTA2Match_570";
+    String TEST_API_URL = "IDOTA2Match_205790";
+    String API_URL = "IDOTA2Match_570";
     String TEST_METHOD = "GetMatchHistory";
     HttpClient client;
     String STEAM_API_URL = "https://api.steampowered.com/";
 
-    public SteamServer(String API_KEY) {
-        this.API_KEY=API_KEY;
-        client = new DefaultHttpClient();
+    public SteamServer(String API_KEY, HttpClient client) {
+        this.API_KEY = API_KEY;
+        this.client = client;
 
     }
 
@@ -28,7 +27,6 @@ public class SteamServer {
 
         HttpGet request = new HttpGet(STEAM_API_URL + TEST_API_URL + "GetMatchHistory" + "/V001/?key=" + API_KEY);
         HttpResponse response = client.execute(request);
-
 
 
         return null;  //To change body of created methods use File | Settings | File Templates.
--- src/test/java/SteamServerTests.java
+++ src/test/java/SteamServerTests.java
@@ -1,9 +1,17 @@
-import org.junit.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.junit.Test;
 import org.wrapdota.game.DotaMatch;
 import org.wrapdota.server.SteamServer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class SteamServerTests {
 
@@ -11,7 +19,16 @@ public class SteamServerTests {
     public void itGetsLatestMatches() throws IOException {
 
         //given
-        SteamServer steamServer = new SteamServer("7C5DA0C928D213C67156C62E56CC1117");
+        HttpClient mockHTTPClient = mock(HttpClient.class);
+
+        List<DotaMatch> mockedList = new ArrayList<DotaMatch>();
+        for (int count = 0; count < 26; count++) {
+            mockedList.add(count, new DotaMatch());
+        }
+
+
+        given(mockHTTPClient.execute(any(HttpGet.class))).willReturn(any(HttpResponse.class));
+        SteamServer steamServer = new SteamServer("7C5DA0C928D213C67156C62E56CC1117", mockHTTPClient);
 
         //when
         List<DotaMatch> matches = steamServer.getLatestMatches();
